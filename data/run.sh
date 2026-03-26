9d37de54c1b69267f686b989032a7ceff84d60fb
Kyu Woong Lee <kwooler@gmail.com>
2026-03-26 14:01:37 -0700

    fix: update run.sh for two-file (main_write/main_read) structure

--- data/run.sh
+++ data/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-g++ -Wall -Wextra --std=c++17 main.cpp -o main 
-timeout 10 ./main < data/data1.txt > result1.txt
-timeout 10 ./main < data/data2.txt > result2.txt
-timeout 10 ./main < data/data3.txt > result3.txt
-timeout 10 ./main < data/data4.txt > result4.txt
+g++ -Wall -Wextra --std=c++17 main_write.cpp -o main_write
+g++ -Wall -Wextra --std=c++17 main_read.cpp -o main_read
+timeout 10 ./main_write < data/data1.txt > /dev/null && timeout 10 ./main_read > result1.txt
+timeout 10 ./main_write < data/data2.txt > /dev/null && timeout 10 ./main_read > result2.txt
+timeout 10 ./main_write < data/data3.txt > /dev/null && timeout 10 ./main_read > result3.txt
+timeout 10 ./main_write < data/data4.txt > /dev/null && timeout 10 ./main_read > result4.txt
+
